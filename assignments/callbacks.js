1de4e4c437d5c3026e16c48ef54e0ae0ceeea1e9
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-07 21:50:35 +0100

    Complete callbacks.js stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -76,7 +76,10 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-   
-
-  
+  noDupes = [];
+  array.forEach((element) =>{
+    if (!noDupes.includes(element)){noDupes.push(element)}})
+  return cb(noDupes);
 }
+
+removeDuplicates([1,1,1,2,3,3,4,5,7,7,3], (uniqueElements) => console.log(uniqueElements));
