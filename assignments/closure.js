183959db602724e4355bc456cfb54a9092a5ef1a
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-07 14:02:29 +0100

    Complete closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let myName = "Emily";
+let yourName = "Bob";
+greeter = () => console.log(`Hello ${yourName}! My name is ${myName}.`);
 
+//greeter();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
